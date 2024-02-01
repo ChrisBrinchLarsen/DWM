33ad37aa13d4630d0ad5e34a23ad98312edf857d
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2024-02-01 16:14:27 +0100

    ayo

--- config.h
+++ config.h
@@ -2,47 +2,26 @@
 #import "masterConfig.h"
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 8;        /* gaps between windows */
-//static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int snap      = 5;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "RobotoMono Nerd Font Propo:style=Bold:size=13" };
-static const char dmenufont[]       = "Fira Code:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const unsigned int baralpha  = 0xED; // 90% opaque
+static const unsigned int borderpx  = borderSize;        /* border pixel of windows */
+static const unsigned int gappx     = gapSize;        /* gaps between windows */
+static const unsigned int snap      = snapTicks;       /* snap pixel */
+static const int showbar            = barEnabled;        /* 0 means no bar */
+static const int topbar             = barAtTop;        /* 0 means bottom bar */
+static const char *fonts[]          = { dwmFont };
+// static const char *dmenufont         = dmenuFont; // We don't use this but represses an error
+static const unsigned int baralpha  = dwmAlpha; // 90% opaque
 static const unsigned int borderalpha = OPAQUE;
 
-static const char lightGreen[]       = "#30FFA4";
-static const char almostLightGreen[] = "#5FBE92";
-static const char darkGreen[]        = "#031624";
-static const char almostDarkGreen[]  = "#044B4A";
-
-/* Custom Colors */
-static const char active_border_col[]       = "#30FFA4";
-static const char inactive_border_col[]     = "#031624";
-static const char active_menu_col[]         = "#30FFA4";
-static const char inactive_menu_col[]       = "#031624";
-static const char active_foreground_col[]   = "#031624";
-static const char inactive_foreground_col[] = "#30FFA4";
-
-
-//static const char *colors[][3]      = {
-//	/*               fg         bg         border   */
-//	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-//	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-//};
+// static const char lightGreen[8]       = mainColorLight;
+// static const char almostLightGreen[8] = mainColorLessLight;
+// static const char darkGreen[8]        = mainColorDark;
+// static const char almostDarkGreen[8]  = mainColorLessDark;
 
 static const char *colors[][3]      = {
 	/*               fg                       bg                  border            */
-	[SchemeNorm] = { inactive_foreground_col, inactive_menu_col,  inactive_border_col},
-	[SchemeSel]  = { active_foreground_col, active_menu_col,  active_border_col      },
-	[SchemeTitle]= { inactive_foreground_col, inactive_menu_col, inactive_menu_col     },
+	[SchemeNorm] = { mainColorLight, mainColorDark,   mainColorDark  },
+	[SchemeSel]  = { mainColorDark,  mainColorLight,  mainColorLight },
+	[SchemeTitle]= { mainColorLight, mainColorDark,   mainColorDark  },
 };
 static const unsigned int alphas[][3] = {
 	/*               fg      bg        border     */
@@ -53,7 +32,7 @@ static const unsigned int alphas[][3] = {
 
 
 /* tagging */
-static const char *tags[] = { "", "", "", "󰺵", "󰙯", "󰎆", "", "", "" };
+static const char *tags[] = { ws1, ws2, ws3, ws4, ws5, ws6, ws7, ws8, ws9 };
 
 /* Lockfile */
 static char lockfile[] = "/tmp/dwm.lock";
@@ -85,9 +64,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "󰝘",      tile },    /* first entry is default */
-	{ "󱨈",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ tiledIcon,      tile },    /* first entry is default */
+	{ floatingIcon,   NULL },    /* no layout function means floating behavior */
+	{ monocleIcon,    monocle },
 };
 
 /* key definitions */
@@ -102,11 +81,15 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+// Look at these two below to understand how to make commands that spawn on a specific monitor
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", almostLightGreen, "-nf", almostDarkGreen, "-sb", lightGreen, "-sf", darkGreen, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dwmFont[0], "-nb", mainColorLessLight, "-nf", mainColorLessDark, "-sb", mainColorLight, "-sf", mainColorDark, NULL };
+
+
+
+static const char *termcmd[]  = { terminal, NULL };
 static const char *dmenucmdnew[]  = { "dmenu_run", NULL};
-static const char *browsercmd[]   = { "librewolf", NULL};
+static const char *browsercmd[]   = { browser, NULL};
 static const char *qalccmd[]      = { "=", NULL};
 
 static const Key keys[] = {
